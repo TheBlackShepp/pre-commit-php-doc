0ba20ca4330ac37ef122b0ffa30afc7152b3c011
TheBlackShepp <orablagh@gmail.com>
2024-09-10 10:20:22 +0200

    Improved FileAttribute to extract the name and type of the attribute

--- php/Files/PartsCode/FileAttribute.php
+++ php/Files/PartsCode/FileAttribute.php
@@ -4,19 +4,53 @@ class FileAttribute
 {
     const REGEX = '(public|protected|private|abstract|readonly)\s*(static\s+)?(\w+)\s*\$([\w_]+)\s*';
 
+    private string $_name;
+    private string $_type;
+
     public function __construct(string $attribute)
+    {
+        // Excecute expression to capture the name and type
+        $matches = $this->_doRegexToMethod($attribute);
+
+        if (count($matches) > 0 &&  count($matches[0]) > 0 ) {
+            $this->_name = $this->_extractName($matches[0]);
+            $this->_type = $this->_extractType($matches[0]);
+        } else {
+            echo "No attributes found.";
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->_name;
+    }
+
+    public function getType(): string
+    {
+        return $this->_type;
+    }
+
+    private function _doRegexToMethod(string $attribute): array
     {
         // Regular expression to capture the attribute details
         $attributePattern = '/\s*' . self::REGEX . '/';
 
-        // Apply the regular expression to extract all attributes
+        // Apply the regular expression to extract the method name and parameters
         if (preg_match_all($attributePattern, $attribute, $matches, PREG_SET_ORDER)) {
-            foreach ($matches as $attribute) {
-                $typeHint = $attribute[3] ?? 'mixed';                               // Default to 'mixed' if not specified
-                $attributeName = $attribute[4];
-            }
-        } else {
-            echo "No attributes found.";
+            return $matches;
         }
+
+        return array();
+    }
+
+    private function _extractName(array $matchesRegex): string
+    {
+        return $matchesRegex[4];
+    }
+
+
+    private function _extractType(array $matchesRegex): string
+    {
+        return $matchesRegex[3];
     }
 }
